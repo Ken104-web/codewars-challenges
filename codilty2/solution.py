66f9328f1bbcf40716a0a13b4f02b56458bc81a7
KenCorp <kennethwarui35@gmail.com>
2023-10-10 12:41:27 +0300

    minOpperation method

---
+++ codilty2/solution.py
@@ -0,0 +1,18 @@
+# python code to count  how many first  elements are the same as the last element in S 
+# check if the first and last char of s are the same
+def countSame(s):
+    count = 0
+    N=len(s)
+
+    for i in range(N):
+        # check if the two elements are the same
+        if s[0] == s[-1]:
+            count += 1
+            # planning of using slice method to remove the first element through slicing the second to the end
+
+    return count
+
+
+
+    
+
