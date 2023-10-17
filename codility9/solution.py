84f706a1f05e5bccb4d26116af85a6208a2a8102
KenCorp <kennethwarui35@gmail.com>
2023-10-17 22:14:40 +0300

    Codility9

--- codility9/solution.py
+++ codility9/solution.py
@@ -1,15 +1,21 @@
-# total value of rolls  = mean(4) * total number of known rolls(N) plus forgo
+# total value of rolls  = mean(4) * total number of known rolls(N) plus F
 # number of rolls = N  + missing rolls(F) N + F = dice rolled
 # sum if dice rolled = (N + F) * M(mean)
 # N dice told eg= [3, 2, 4, 3] number of rolls = 4
 # F dice not told
-# meaning that if we subtracted sum of array A or N from total value of rolls we get F(missing) 
+# meaning that if we subtracted sum of array A or N from total value of rolls we get F(missing)
+# so [3, 2, 4, 3] = 12 from total value = 24 we 12 which is F
+# so find values of F 
 def solution(A, F, M):
     N = len(A)
     total_value_rolls = M * (N + F)
     total_sum_known = sum(A)
     left = total_value_rolls - total_sum_known
 
-  
+    # if eg: value of F ==  17 and F = 2
+    # 17/2 not possible since highest number in dice is 6  
+    if left <= 0 or left < F:
+        return []
+    
     
     
