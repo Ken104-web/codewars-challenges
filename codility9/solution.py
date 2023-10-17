8395960992d48718c9d0b4273e0f57a79ccf7e89
KenCorp <kennethwarui35@gmail.com>
2023-10-17 21:35:58 +0300

    Codility9

--- codility9/solution.py
+++ codility9/solution.py
@@ -3,6 +3,7 @@
 # sum if dice rolled = (N + F) * M(mean)
 # N dice told eg= [3, 2, 4, 3] number of rolls = 4
 # F dice not told
+# meaning that if we subtracted sum of array A or N from total value of rolls we get F(missing) 
 def solution(A, F, M):
     N = len(A)
     total_value_rolls = M * (N + F)
