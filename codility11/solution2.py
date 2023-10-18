aa5ca7c994b2b0fdd23e609949c353ef058a5b63
KenCorp <kennethwarui35@gmail.com>
2023-10-18 21:38:34 +0300

    Codility11(2)

--- codility11/solution2.py
+++ codility11/solution2.py
@@ -4,4 +4,14 @@ def solution(S):
     max_sum = 0
 
     for i in range(N):
-        pass
+        combined_values = int(S[i] + S[i + 1])
+        combined_values2 = int(S[i] + S[i + 1])
+
+        max_result = combined_values + combined_values2
+
+        if max_result > max_sum:
+            max_sum = max_result
+
+    return max_sum
+
+
