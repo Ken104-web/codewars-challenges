cd42b69786a8e0c12863ad1577f92e0da9e9652d
KenCorp <kennethwarui35@gmail.com>
2023-12-12 13:20:55 +0300

    done

--- leetcode1/solution.py
+++ leetcode1/solution.py
@@ -1,2 +1,11 @@
-def solution():
-    pass
+# find the  two largest numbers of the  input array 
+class Solution:
+    def maxProduct(self, nums: [int]) -> int:
+        r = 0
+        current_max = nums[0]
+
+        for i in range(1, len(nums)):
+            r = max(r, (current_max - 1) * (nums[i] - 1))
+            current_max = max(current_max, nums[i])
+        
+        return r
---
+++ leetcode1/test.py
@@ -0,0 +1,4 @@
+from solution import Solution
+nums = [3, 4, 5, 2]
+
+print(Solution(), (nums))
