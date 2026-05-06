547f7bdfd90a6afeea6dbcd2cc20296bd7898ea2
kenneth <kennethwarui35@gmail.com>
2026-05-06 23:15:20 +0300

    retracing postgres

---
+++ postgres/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String nullable=False)
+    last_name = db.Column(db.String nullable=False)
+    email = db.Column(db.String unique=True)
+
