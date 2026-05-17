616ff7fd0aa5a4becf1da3dd1ec70fa1376397a4
kenneth <kennethwarui35@gmail.com>
2026-05-17 21:07:58 +0300

    what a run and called it a success

--- postgres/app.py
+++ postgres/app.py
@@ -33,7 +33,8 @@ class CustomerByID(Resource):
 api.add_resource(CustomerByID, '/customer/<int:id>')
 
 
-
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
 
 
 
--- postgres/models.py
+++ postgres/models.py
@@ -9,6 +9,14 @@ class Customer(db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True)
-    
+
+    def to_dict(self):
+        return {
+                "id": self.id,
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "email": self.email
+                }
     def __repr__(self):
         return f'<Customer {self.first_name} {self.last_name} | email: {self.email}>'
+
